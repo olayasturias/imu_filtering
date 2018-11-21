3df888b6c2c6a3a6b9f268a2270bcd5fd545b169
olayasturias <oalvarez@ing.uc3m.es>
2018-11-21 16:57:13 +0100

    rotated axis of imu

--- src/imu_filter.py
+++ src/imu_filter.py
@@ -11,21 +11,24 @@ import csv
 #import lowpass
 from scipy.signal import butter, lfilter, freqz
 import pywt
+import tf2_ros
 from statsmodels.robust import mad
 from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, TransformStamped
 from scipy import integrate
 
 
 class Imu_Filter():
-    def __init__(self,topic = '/imu'):
+    def __init__(self,topic = '/imu', base_frame = 'world', imu_frame = 'base_link'):
         # Unfiltered accelerations
         self.linear_accs = [[],[],[]]
         # Unfiltered rotations
-        self.gyro = [[],[],[]]
+        self.gyro = [[],[],[],[]]
         # Time array
         self.t = []
-
+        # Frames
+        self.base_frame = base_frame
+        self.imu_frame = imu_frame
         # Filtered accelerations
         self.filtered_accs = [[],[],[]]
 
@@ -44,9 +47,20 @@ class Imu_Filter():
 
     def imu_callback(self, data):
         # Add linear accelerations to array for later processing
-        self.linear_accs[0].append(data.linear_acceleration.x)
-        self.linear_accs[1].append(data.linear_acceleration.y)
+        # self.linear_accs[0].append(data.linear_acceleration.x)
+        # self.linear_accs[1].append(data.linear_acceleration.y)
+        # self.linear_accs[2].append(data.linear_acceleration.z)
+
+        self.linear_accs[0].append(data.linear_acceleration.y)
+        self.linear_accs[1].append(-data.linear_acceleration.x)
         self.linear_accs[2].append(data.linear_acceleration.z)
+
+        # Add quaternion orientation (directly)
+        self.gyro[0].append(data.orientation.x)
+        self.gyro[1].append(data.orientation.y)
+        self.gyro[2].append(data.orientation.z)
+        self.gyro[3].append(data.orientation.w)
+
 
         self.t.append(rospy.get_rostime().secs)
 
@@ -82,16 +96,44 @@ class Imu_Filter():
     def publish(self, pub, type, data):
 
         msg = type
+
+        x = data[0][len(data[0])-1]
+        y = data[1][len(data[1])-1]
+        z = data[2][len(data[2])-1]
+
+        ox = self.gyro[0][len(self.gyro[0])-1]
+        oy = self.gyro[1][len(self.gyro[1])-1]
+        oz = self.gyro[2][len(self.gyro[2])-1]
+        ow = self.gyro[3][len(self.gyro[3])-1]
+
         if msg == Imu():
             msg.header.stamp = rospy.Time.now()
-            msg.linear_acceleration.x = data[0][len(data[0])-1]
-            msg.linear_acceleration.y = data[1][len(data[1])-1]
-            msg.linear_acceleration.z = data[2][len(data[2])-1]
+            msg.linear_acceleration.x = x
+            msg.linear_acceleration.y = y
+            msg.linear_acceleration.z = z
 
         elif msg == Pose():
-            msg.position.x = data[0][len(data[0])-1]
-            msg.position.y = data[1][len(data[0])-1]
-            msg.position.z = data[2][len(data[0])-1]
+            msg.position.x = x
+            msg.position.y = y
+            msg.position.z = z
+
+        elif msg == TransformStamped():
+            msg.header.stamp = rospy.Time.now()
+            msg.header.frame_id = self.base_frame
+            msg.child_frame_id = self.imu_frame
+
+            msg.transform.translation.x = x
+            msg.transform.translation.y = y
+            msg.transform.translation.z = z
+
+            msg.transform.rotation.x = ox
+            msg.transform.rotation.y = oy
+            msg.transform.rotation.z = oz
+            msg.transform.rotation.w = ow
+
+            pub.sendTransform(msg)
+
+            return # Because tf doesnt use publish
 
         pub.publish(msg)
 
@@ -141,6 +183,7 @@ class Imu_Filter():
 
 
 
+
 if __name__=='__main__':
     rospy.init_node('imu_filter_wav',anonymous = True, log_level=rospy.DEBUG)
 
@@ -150,16 +193,20 @@ if __name__=='__main__':
     rospy.logdebug("WAVELET FILTER MODE %s", wav_mode)
     wav_level = rospy.get_param('~wavelet_level')
     rospy.logdebug("WAVELET FILTER LEVEL %s", wav_level)
+    base_frame = rospy.get_param('~base_frame')
+    rospy.logdebug("BASE FRAME %s", base_frame)
+    imu_frame = rospy.get_param('~imu_frame')
+    rospy.logdebug("IMU FRAME %s", imu_frame)
 
-    iwf = Imu_Filter(topic=imutopic)
+    iwf = Imu_Filter(topic=imutopic, base_frame = base_frame, imu_frame = imu_frame)
 
     # Data publishers
     imu_filtered_pub    = rospy.Publisher('/imu_filtered', Imu, queue_size=10)
     pose_filtered_pub   = rospy.Publisher('/imu_pose', Pose, queue_size=10)
     pose_unfiltered_pub = rospy.Publisher('/unfiltered_imu_pose', Pose, queue_size=10)
+    tf_pub              = tf2_ros.TransformBroadcaster()
 
-
-    rate = rospy.Rate(1) # 1 sec
+    rate = rospy.Rate(2)
     while not rospy.is_shutdown():
         rate.sleep() # sleeps for 1 sec
 
@@ -174,6 +221,8 @@ if __name__=='__main__':
         rospy.logdebug("Position x after filtering: %f", iwf.position[0][0])
         # Publish filtered pose
         iwf.publish(pose_filtered_pub, Pose(), iwf.position)
+        # Publish tf with filtered pose
+        iwf.publish(tf_pub, TransformStamped(), iwf.position)
 
         rospy.logdebug(" UNfiltered acc x example value: %f", iwf.linear_accs[0][0])
         # Integrate unfiltered accs
